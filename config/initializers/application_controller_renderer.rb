a040b5f45d339d41ce6de4da52cc84efc4d19e93
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-08 21:19:16 +0900

    crud studied

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8c1abf0e573f23b6871cac143ce942cbe77533512109746b44d469afce0ccb118647ec152108f81a8c9e43dcf31c735d6c58dd66ff50a484abcf90e194d764f9'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_104236) do
+ActiveRecord::Schema.define(version: 2019_10_08_104225) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "body"
+    t.string "image_id"
   end
 
 end
